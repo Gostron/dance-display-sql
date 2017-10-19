e203f08ad91392974ca0d28c8a02c4ff544f843f
Gostron <MX5273@D29.tes.local>
2017-10-19 17:49:08 +0200

    update

--- app/service/select-all.js
+++ app/service/select-all.js
@@ -59,8 +59,33 @@ const SQL_SELECT_ALL_OBJECTS_TABLES = {
 const SQL_SELECT_ALL =
   'SELECT {{fields}} FROM {{table}} WHERE id > {id} LIMIT {results}'
 
+function reworkObject (object, labels) {
+  // For each property of the object
+  for (let key in object) {
+    // And each label
+    _.each(labels, function (label) {
+      // If the property begins with the label, reunite it in a sub-object
+      if (key.indexOf(label) === 0) {
+        var newKey = key.length > label.length ? key.substring(label.length + 1) : 'name'
+        if (!object[label]) object[label] = {}
+        object[label][newKey] = object[key]
+        delete object[key]
+      }
+    })
+    // Recursively for arrays
+    if (Array.isArray(object[key])) {
+      for (let i = 0; i < object[key].length; i++)
+      object[key][i] = reworkObject(object[key][i], labels)
+    }
+    // Recursively for sub-objects
+    else if (Object.prototype.toString.call(object[key]) === '[object Object]') {
+      object[key] = reworkObject(object[key], labels)
+    }
+  }
+  return object
+}
 
-function sqlFilter (sql, id, competitionId, filterId, filterCompetitionId) {
+function generalSqlFilter (sql, id, competitionId, filterId, filterCompetitionId) {
   var filters = []
   if (id && filterId) filters.push(filterId + ' = {id}')
   if (competitionId && filterCompetitionId) filters.push(filterCompetitionId + ' = {competitionId}')
@@ -111,6 +136,11 @@ const SQL_VIEW_LIST = {
     sql: 'SELECT * FROM ref_dance',
     nestTables: false,
   },
+  // AGE QUERY
+  ref_age: {
+    sql: 'SELECT * FROM ref_age',
+    nestTables: false,
+  },
   // CLEARANCE QUERY
   ref_clearance: {
     sql: 'SELECT * FROM ref_clearance',
@@ -131,6 +161,16 @@ const SQL_VIEW_LIST = {
     sql: 'SELECT * FROM judge',
     nestTables: false,
   },
+  // CONTESTANT QUERY
+  contestant: {
+    sql: 'SELECT * FROM contestant',
+    nestTables: false,
+  },
+  // COUPLE QUERY
+  couple: {
+    sql: 'SELECT * FROM couple',
+    nestTables: false,
+  },
   // CATEGORY QUERY
   category: {
     sql: 'SELECT * FROM category\
@@ -141,7 +181,7 @@ const SQL_VIEW_LIST = {
       LEFT JOIN view_category_judge          ON view_category_judge.id_category   = category.id\
       LEFT JOIN view_category_subscription   ON view_category_subscription.id_category = category.id',
     sqlFilter: function (sql, id, competitionId) {
-      sqlFilter(sql, id, competitionId, 'category.id', 'category.id_competition')
+      return generalSqlFilter(sql, id, competitionId, 'category.id', 'category.id_competition')
     },
     nestTables: true,
     nestingOptions: [
@@ -184,25 +224,27 @@ const SQL_VIEW_LIST = {
         delete category.view_competition
         return category
       })
-    }
+    },
+    reworkLabels: ['male', 'female', 'current_dance', 'current_event', 'current_stage', 'current_category']
   },
   // COMPETITION QUERY
   competition: {
     sql: 'SELECT * FROM view_competition',
     nestTables: false,
     sqlFilter: function (sql, id) {
-      sqlFilter(sql, id, null, 'category.id')
+      return generalSqlFilter(sql, id, null, 'id')
     },
+    reworkLabels: ['current_dance', 'current_event', 'current_stage', 'current_category']
   },
   // EVENT QUERY
   event: {
     sql: 'SELECT * FROM view_event\
-      LEFT JOIN view_stage_dancer stage_d    ON view_event.id_stage    = stage_d.id\
+      LEFT JOIN view_stage_dancer stage_d    ON view_event.id_stage    = stage_d.id_stage\
       LEFT JOIN category_dance               ON view_event.id_category = category_dance.id\
       LEFT JOIN view_category_judge          ON view_event.id_category = view_category_judge.id_category\
       LEFT JOIN view_category_subscription   ON view_event.id_category = view_category_subscription.id_category',
     sqlFilter: function (sql, id, competitionId) {
-      sqlFilter(sql, id, competitionId, 'view_event.id', 'view_event.id_competition')
+      return generalSqlFilter(sql, id, competitionId, 'view_event.id', 'view_event.id_competition')
     },
     nestTables: true,
     nestingOptions: [
@@ -216,6 +258,51 @@ const SQL_VIEW_LIST = {
       // TODO
       return results
     }
+  },
+  // STAGE QUERY
+  stage: {
+    sql: 'SELECT * FROM view_stage\
+      LEFT JOIN view_stage_dancer stage_d    ON view_stage.id          = stage_d.id_stage\
+      LEFT JOIN category_dance               ON view_stage.id_category = category_dance.id_category\
+      LEFT JOIN view_category_judge          ON view_stage.id_category = view_category_judge.id_category\
+      LEFT JOIN view_category_subscription   ON view_stage.id_category = view_category_subscription.id_category',
+    sqlFilter: function (sql, id, competitionId) {
+      return generalSqlFilter(sql, id, competitionId, 'view_stage.id', 'view_stage.id_competition')
+    },
+    nestTables: true,
+    nestingOptions: [
+      { tableName: 'view_stage',                  pkey: 'id'},
+      { tableName: 'stage_d',                     pkey: 'id', fkeys: [{ table: 'view_stage', col: 'id_stage' }]},
+      { tableName: 'category_dance',              pkey: 'id', fkeys: [{ table: 'view_stage', col: 'id_category' }]},
+      { tableName: 'view_category_judge',         pkey: 'id', fkeys: [{ table: 'view_stage', col: 'id_category' }]},
+      { tableName: 'view_category_subscription',  pkey: 'id', fkeys: [{ table: 'view_stage', col: 'id_category' }]}
+    ],
+    simplify: function (results) {
+      return _.map(results, function (stage) {
+        stage.dancers = _.map(stage.stage_d, function (couple) {
+          delete couple.id_category
+          delete couple.id_competition
+          return couple
+        })
+        stage.category_subscriptions = _.map(stage.view_category_subscription, function (couple) {
+          delete couple.id_category
+          delete couple.id_competition
+          return couple
+        })
+        stage.judges = _.map(stage.view_category_judge, function (judge) {
+          delete judge.id_category
+          return judge
+        })
+        stage.dances = _.map(_.sortBy(stage.category_dance, 'index'), 'dance')
+        delete stage.stage_d
+        delete stage.view_category_subscription
+        delete stage.view_category_judge
+        delete stage.category_dance
+        delete stage.id_competition
+        return stage
+      })
+    },
+    reworkLabels: ['category', 'male', 'female']
   }
 }
 
@@ -262,6 +349,7 @@ module.exports.execute = function (options) {
 module.exports.testView = function (options, query) {
 
   var options = SQL_VIEW_LIST[options.object]
+  if (!options) return Promise.reject('Impossible de trouver l\'objet \'' + options.object + '\' dans la liste.')
   var sql = options
   var parameters = null
   if (options.sqlFilter) {
@@ -274,11 +362,19 @@ module.exports.testView = function (options, query) {
     .then(function (conn) {
       return conn.query(sql, parameters)
         .then(function (results) {
-          if (options.nestTables && options.nestingOptions) {
-            if (options.simplify)
-              return options.simplify(nester.convertToNested(results, options.nestingOptions))
-            else return nester.convertToNested(results, options.nestingOptions)
-          } else return results
+          var output = results
+          /// Results obtained here
+          // Filter for nestification
+          if (options.nestTables && options.nestingOptions)
+            output = nester.convertToNested(results, options.nestingOptions)
+          // Additional simplification (deleting props, etc)
+          if (options.simplify)
+            output = options.simplify(output)
+          // Property reunification in object
+          if (options.reworkLabels)
+            output = _.map(output, function (result) { return reworkObject(result, options.reworkLabels) })
+
+          return output
         })
         .finally(function () {
           conn.release()
